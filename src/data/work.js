d13c0d36f699a4918b2cdfd28c64fc40c22ec274
Vishalll069 <vishaldurge567@gmail.com>
2023-10-25 10:23:34 +0530

    work updated

--- src/data/work.js
+++ src/data/work.js
@@ -6,11 +6,10 @@ export default function getWork() {
         companyLink: "https://www.clayfin.com/",
         position: "Software Developer",
         points: [
-          "Working on Employee And HR management Mobile App",
+          "Working on Employee And HR operations management Mobile App",
           "Testing UI for different devices.",
           "Applying best design practices.",
-          "Creating beautiful and intuitive interfaces Google's latest and Material Design guidelines.",
-          
+          "Creating beautiful and intuitive interfaces Google's latest and Material Design guidelines.",          
         ],
         time:'08/23 - Current'
       },
@@ -22,8 +21,7 @@ export default function getWork() {
         points: [
           "Worked on Exam Portal web App",
           "Applied best design practices.",
-          "Created beautiful and intuitive interfaces Material Design guidelines.",
-          
+          "Created beautiful and intuitive interfaces Material Design guidelines.",          
         ],
         time:'05/22 - 07/22'
       },
